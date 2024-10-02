43d8d375cfab67495bc25f453d5618e16ad1004a
DevinDaMonki <dmryan237@gmail.com>
2024-10-01 18:39:04 -1000

    First Commit

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,28 +4,12 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class ExampleSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  private CANSparkMax motor1;
-  private CANSparkMax motor2;
-  private GenericHID controller;
-
-  public ExampleSubsystem(GenericHID controller) {
-    this.controller = controller;
-    motor1 = new CANSparkMax(7, MotorType.kBrushless);
-    motor2 = new CANSparkMax(10, MotorType.kBrushless);
-  }
-
-  public void setPower(double power) {
-    motor1.set(power);
-    motor2.set(power);
-  }
+  public ExampleSubsystem() {}
 
   /**
    * Example command factory method.
@@ -54,7 +38,6 @@ public class ExampleSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    setPower(1);
   }
 
   @Override
