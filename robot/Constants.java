273446de4720be7e62f3e7a4275ad73f3db2368e
BeginnerCoder101 <136760927+BeginnerCoder101@users.noreply.github.com>
2024-10-21 18:26:55 -1000

    10/21/24 - 3

---
+++ robot/Constants.java
@@ -0,0 +1,64 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+
+public static class FlywheelConstants{
+  public static final int kFeedFlywheelId = 6;
+  public static final int kShootFlywheelId = 11;
+
+  public static final double kScoringSpeed = 100;
+  public static final double kEjectSpeed = 25;
+  public static final double kFeedSpeed = 0.1;
+}
+
+  public static class GamePadConstants {
+    public static final int kPrimaryGamepadPort = 0;
+    public static final int kSecondaryGamepadPort = 1;
+    public static final int kTestingGamepadPort = 2;
+
+    //Gamepad Axis Ports
+    public static final int kleftXJoystickPort = 0;
+    public static final int kLeftYJoystickPort = 1;
+    public static final int kRightXJoystickPort = 4;
+    public static final int kRightYJoystickPort = 5; 
+    public final static int kLeftTriggerPort = 2;
+    public final static int kRightTriggerPort = 3;
+
+    //Gamepad Button Ports
+    public final static int kAButtonPort = 1;
+    public final static int kBButtonPort = 2;
+    public final static int kXButtonPort = 3;
+    public final static int kYButtonPort = 4;
+    public final static int kLeftBumperPort = 5;
+    public final static int kRightBumperPort = 6;
+    public final static int kBackButtonPort = 7;
+    public final static int kStartButtonPort = 8;   
+
+    //joystick port for the gamepad
+    public final static int kPrimaryLeftStickPort = 0;
+    public final static int kPrimaryRightStickPort = 1;
+
+    //Dpad values
+    public final static int kDpadUp = 0;
+    public final static int kDpadDown = 180;
+
+    //Deadzone value
+    public final static double kDeadzone = 0.2;
+
+  }
+}
---
+++ robot/commands/ExampleCommand.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.GenericHID;
+import frc.robot.Constants.GamePadConstants;
+
+/** An example command that uses an example subsystem. */
+public class ExampleCommand extends Command {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final ExampleSubsystem m_subsystem;
+  private final GenericHID controller;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public ExampleCommand(ExampleSubsystem subsystem, GenericHID controller) {
+    m_subsystem = subsystem;
+    this.controller = controller;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    // A is 1
+    // B is 2
+    if (controller.getRawButton(GamePadConstants.kAButtonPort)) {
+    m_subsystem.setPower(1);
+  }
+    else if (controller.getRawButton(GamePadConstants.kBButtonPort)) {
+    m_subsystem.setPower(-1);
+  }
+  else {
+    m_subsystem.setPower(0);
+  }
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
